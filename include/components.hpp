2671f994dc532094b08c849e870552636c74230b
Victor Berbenets <berbenets.vd@phystech.edu>
2023-08-26 10:26:42 +0300

    operator== and checking for equal points

--- include/components.hpp
+++ include/components.hpp
@@ -10,12 +10,13 @@ bool is_equal(double val1, double val2) {
 struct point_t {
     point_t(double x, double y, double z):
             x_{x}, y_{y}, z_{z} {};
+    ~point_t() = default;
 
     bool is_valid() const noexcept {
         return x_ != NAN && y_ != NAN && z_ != NAN;
     };
 
-    bool equal(const point_t& other) const noexcept {
+    bool operator==(const point_t& other) const noexcept {
         return is_equal(x_, other.x_) &&
                is_equal(y_, other.y_) &&
                is_equal(z_, other.z_);
@@ -28,9 +29,11 @@ struct point_t {
     double x_ = NAN, y_ = NAN, z_ = NAN;
 };
 
-class line_t {     // line view:   ax + by + cz + d = 0
-public:
+struct line_t {     // line view:   a_x + b_y + c_z + d_ = 0
     line_t(const point_t& pt1, const point_t& pt2) {
+        if (pt1 == pt2) {
+            throw std::invalid_argument{"points are equal, can't create 'line_t'"};
+        }
         // finding cordinates of the dirrecting vector
         double a_dirr = pt1.x_ - pt2.x_;
         double b_dirr = pt1.y_ - pt2.y_;
@@ -40,29 +43,26 @@ public:
             a_ = -(b_dirr/a_dirr + c_dirr/a_dirr);
             b_ = c_ = 1;
         } else if (!is_equal(b_dirr, 0)) {
-            b_ = -(a_dirr/b_dirr + c_dirr/b_dirr);   
+            b_ = -(a_dirr/b_dirr + c_dirr/b_dirr);
             a_ = c_ = 1;
-        } else if (!is_equal(c_dirr, 0)) {
+        } else {
             c_ = -(a_dirr/c_dirr + b_dirr/c_dirr);
             a_ = b_ = 1;
-        } else {
-            std::cout << "point's are equal\n";
-            valid = false;
         }
-        if (valid) {
-            d_ = -(a_ * pt1.x_ + b_ * pt1.y_ + c_ * pt1.z_);
-        }
+        d_ = -(a_ * pt1.x_ + b_ * pt1.y_ + c_ * pt1.z_);
     };
+
+    ~line_t() = default;
 
     void print() {
         std::cout << "a = " << a_ << " b = " << b_ << " c = " << c_
                 << " d = " << d_ << std::endl;
     }
-private: 
-    double a_ = NAN, b_ = NAN, 
-           c_ = NAN, d_ = NAN;
-    bool valid = true;
+//------------------------------------------------------------------// 
+    double a_ = NAN, b_ = NAN, c_ = NAN, d_ = NAN;
 };
+
+
 
 } // <-- namespace yLAB
 
