08d9e589c02acd0cd160346f30f708d69749a7bf
Victor Berbenets <berbenets.vd@phystech.edu>
2023-08-27 22:22:00 +0300

    made it more watchable

--- include/utils.hpp
+++ include/utils.hpp
@@ -7,8 +7,8 @@
 namespace yLAB {
 
 inline double determ(double a, double b, double c, double d) { //|a b|
-    return a * d - c * b;                               //|c d| = a * d - c * b
-};
+    return a * d - c * b;                                      //|c d| = a * d - c * b
+}
 
 inline bool is_equal(double val1, double val2) {
     return std::fabs(val1 - val2) < std::numeric_limits<double>::epsilon();
