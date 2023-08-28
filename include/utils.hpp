9314d2c42fce926079951d6041291dd86ddd0aa8
Victor Berbenets <berbenets.vd@phystech.edu>
2023-08-28 15:09:50 +0300

    made scalar multiply

--- include/utils.hpp
+++ include/utils.hpp
@@ -26,6 +26,10 @@ inline coords_t calc_vects_product(const coords_t& vec1, const coords_t& vec2) {
              determ(vec1[0], vec1[1], vec2[0], vec2[1]), };
 }
 
+inline double scalar_multiply(const coords_t& vec1, const coords_t& vec2) {            
+    return vec1[0] * vec2[0] + vec1[1] * vec2[1] + vec1[2] * vec2[2];
+}
+
 }
 
 #endif
