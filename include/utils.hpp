4f7cfd8bf0a04bd4c79418b66abc561e47c35d1a
Victor Berbenets <berbenets.vd@phystech.edu>
2023-08-30 21:31:18 +0300

    renamed funcions

--- include/utils.hpp
+++ include/utils.hpp
@@ -12,15 +12,9 @@ inline double determ(double a, double b, double c, double d) { //|a b|
     return a * d - c * b;                                      //|c d| = a * d - c * b
 }
 
-inline bool is_equal(double val1, double val2) {
-    static constexpr double BITS_DIFFERENCE = 16; // 4 bits
+inline bool are_equal(double val1, double val2) {
     static constexpr double EPSILON = 1e-9;
-   /* std::cout << "val1 = " << val1 << '\n';
-    std::cout << "val2 = " << val2 << '\n';
-    std::cout << "diff = " << std::fabs(val1 - val2) << '\n';
-    std::cout << "epsilon = " << 16*std::numeric_limits<double>::epsilon() << '\n';
-    std::cout << "is equal result = " << (std::fabs(val1 - val2) < 16*std::numeric_limits<double>::epsilon()) << '\n';*/
-    return std::fabs(val1 - val2) < BITS_DIFFERENCE*EPSILON;
+    return std::fabs(val1 - val2) < EPSILON;
 }
 
 using vector_t = std::array<double, 3>; // each vector has three coordinates
@@ -34,19 +28,19 @@ inline vector_t calc_vects_product(const vector_t& vec1, const vector_t& vec2) {
             -determ(vec1[0], vec1[2], vec2[0], vec2[2]),
              determ(vec1[0], vec1[1], vec2[0], vec2[1]), };
 }
-inline bool is_complanar(const vector_t& vec1, const vector_t& vec2, const vector_t& vec3) {
+inline bool are_complanar(const vector_t& vec1, const vector_t& vec2, const vector_t& vec3) {
     // | a1 a2 a3 |
     // | b1 b2 b3 | = 0
     // | c1 c2 c3 |
-    return is_equal( vec1[0] * determ(vec2[1], vec2[2], vec3[1], vec3[2]) -
-                     vec1[1] * determ(vec2[0], vec2[2], vec3[0], vec3[2]) +
-                     vec1[2] * determ(vec2[0], vec2[1], vec3[0], vec3[1]), 0 );
+    return are_equal( vec1[0] * determ(vec2[1], vec2[2], vec3[1], vec3[2]) -
+                      vec1[1] * determ(vec2[0], vec2[2], vec3[0], vec3[2]) +
+                      vec1[2] * determ(vec2[0], vec2[1], vec3[0], vec3[1]), 0 );
 }
 
 inline bool is_null_vector(const vector_t& vector) {
-    return is_equal(vector[0], 0) &&
-           is_equal(vector[1], 0) &&
-           is_equal(vector[2], 0);
+    return are_equal(vector[0], 0) &&
+           are_equal(vector[1], 0) &&
+           are_equal(vector[2], 0);
 }
 
 inline vector_t get_vector(const point_t& pt1, const point_t& pt2) {
