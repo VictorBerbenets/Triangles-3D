0447aee04d3a7238c9e43969c29b8e9a2faf4a55
Victor Berbenets <berbenets.vd@phystech.edu>
2023-09-03 13:18:39 +0300

    show me what kind of beast you are, oct_tree

--- include/oct_tree.hpp
+++ include/oct_tree.hpp
@@ -1,13 +1,35 @@
 #include <iostream>
+#include <list>
+#include <memory>
 
-class BoandingBox final {
-    
-}
+#include "triangle.hpp"
 
-class OctTree final {
+namespace spaceBreaking {
+
+using namespace yLAB;
+
+class BoandingCube {
 
 public:
 
+protected:
+
 private:
 
-}
+}; // <--- class BoandingCube
+
+class OctTree final: protected BoandingCube {
+
+public:
+
+
+private:
+    std::list<triangle_t> data_;
+    
+    std::unique_ptr<OctTree> left_;
+    std::unique_ptr<OctTree> right_;
+
+    std::unique_ptr<OctTree> parent_;
+}; // <--- class OctTree
+
+} // <--- namespace spaceBreaking
