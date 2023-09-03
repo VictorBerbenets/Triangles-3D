e2ca1476a934129f4046e0b22ead418a733a0334
Victor Berbenets <berbenets.vd@phystech.edu>
2023-09-03 18:30:54 +0300

    some sketches, need to know how to wark with smart pointers)

--- include/oct_tree.hpp
+++ include/oct_tree.hpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <list>
+#include <cmath>
 #include <memory>
 
 #include "triangle.hpp"
@@ -8,28 +9,54 @@ namespace spaceBreaking {
 
 using namespace yLAB;
 
-class BoandingCube {
+class BoundingCube {
+    using size_type = std::size_t;
 
-public:
+    static constexpr size_type MAX_SPACE_DEGREE = 64;
+    static constexpr size_type SPACE_BASE       = 2;
+    static constexpr double MAX_CUBE_SIDE       = std::pow(2, 64);
+    static constexpr double MIN_CUBE_SIDE       = 1;
 
+    void set_center() const;
 protected:
+    BoundingCube();
+    BoundingCube(const point_t& center);
+    ~BoundingCube() = default;
 
+    is_inside(const point_t& pt) const;
+    
 private:
-
+    point_t center_;
+    double side_size_;
 }; // <--- class BoandingCube
 
-class OctTree final: protected BoandingCube {
+class Node final: protected BoundingCube {
+    
+    bool is_limit_reached() const noexcept;
 
-public:
-
-
+    void insert(const triangle_t& tria) const;
+    void construct_new_cubes() const;
 private:
     std::list<triangle_t> data_;
     
-    std::unique_ptr<OctTree> left_;
-    std::unique_ptr<OctTree> right_;
+    std::unique_ptr<Node[]> childs_;
+    std::unique_ptr<Node> parent_;
 
-    std::unique_ptr<OctTree> parent_;
-}; // <--- class OctTree
+
+}; // <--- class Node
+
+class OctTree final {
+    using value_type       = std::unique_ptr<Node>;
+    using const_value_type = const value_type;
+public:
+    OctTree();
+    ~OctTree() = default;
+
+    void insert_triangle(const triangle_t& tria) const;
+    const_value_type get_root_node() const noexcept;
+private:
+    value_type root_node_;
+
+};
 
 } // <--- namespace spaceBreaking
--- src/OctTree/oct_tree.cpp
+++ src/OctTree/oct_tree.cpp
@@ -1,4 +1,17 @@
+
+#include "triangle.hpp"
 #include "oct_tree.hpp"
 
+namespace spaceBreaking {
 
+OctTree::OctTree(): root_node_.parent_(nullptr) {}
 
+OctTree::const_value_type OctTree::get_root_node() const noexcept {
+    return root_node_;
+}
+
+void OctTree::insert_triangle(const triangle_t& tria) const {
+    root_node_.insert(tria);
+}
+
+} // <--- namespace spaceBreaking
