6298c81b229972a01eb75a364c7474c8c69e44b6
Victor Berbenets <berbenets.vd@phystech.edu>
2023-09-04 12:07:22 +0300

    need to make smart recursive function

--- include/oct_tree.hpp
+++ include/oct_tree.hpp
@@ -13,43 +13,50 @@ namespace spaceBreaking {
 using namespace yLAB;
 
 class BoundingCube {
+protected:
     using size_type = std::size_t;
 
     static constexpr size_type MAX_SPACE_DEGREE = 64;
     static constexpr size_type SPACE_BASE       = 2;
-    static constexpr double MIN_CUBE_SIDE       = 1;
-    static constexpr double MAX_CUBE_SIDE       = std::pow(2, 64);
+    static constexpr double MIN_CUBE_SIDE       = 1; // = 2^space_degree
 
-    void set_center() const;
+    void set_center() noexcept;
     bool is_point_inside(const point_t& pt) const;
-protected:
+
     BoundingCube();
-    BoundingCube(const point_t& center, double side_size);
+    BoundingCube(const point_t& center, double space_degree);
     ~BoundingCube() = default;
 
     bool is_tria_inside(const triangle_t& tria) const;
-private:
+    double side_length() const;
+//-----------------------------------------------------------------------//
     point_t center_;
-    double side_size_;
+    double space_degree_;
 }; // <--- class BoandingCube
 
 class Node final: protected BoundingCube {
-    
-    bool is_limit_reached() const noexcept;
+    static constexpr std::size_t VOLUMES_NUMBER = 8;
 
+    bool is_limit_reached() const noexcept;
     void construct_new_cubes() const;
+    void set_pointers();
 public:
-    Node();
-    void insert(const triangle_t& tria) const;
+    Node(const Node& parent);
+    Node(const Node& parent, const point_t& center, double space_degree);
+    ~Node() = default; 
+
+    void insert(const triangle_t& tria);
 private:
     std::list<triangle_t> inside_cube_trias_;
     std::list<triangle_t> intersec_trias_;
 
     std::unique_ptr<Node[]> childs_;
-    std::shared_ptr<Node> parent_;
+    const Node& parent_;
 
+    bool are_ptrs_set_ = false;
 
 }; // <--- class Node
+
 
 class OctTree final {
     using value_type       = Node;
--- src/OctTree/oct_tree.cpp
+++ src/OctTree/oct_tree.cpp
@@ -1,20 +1,58 @@
 
 #include "triangle.hpp"
 #include "oct_tree.hpp"
+#include "utils.hpp"
 
 namespace spaceBreaking {
 
+
 BoundingCube::BoundingCube():
                             center_{0, 0, 0},
-                            side_size_{MAX_CUBE_SIDE} {}
+                            space_degree_{MAX_SPACE_DEGREE} {}
                             
-BoundingCube::BoundingCube(const point_t& center, double side_size):
+BoundingCube::BoundingCube(const point_t& center, double space_degree):
                             center_{center},
-                            side_size_{side_size} {}
+                            space_degree_{space_degree} {}
 
-void Node::insert(const triangle_t& tria) const {
+
+double BoundingCube::side_length() const {
+    return std::pow(2, space_degree_);
+}
+
+bool BoundingCube::is_tria_inside(const triangle_t& tria) const {
+
+}
+
+bool BoundingCube::is_point_inside(const point_t& pt) const {
+
+}
+
+void Node::set_pointers() {
     
 }
+
+void Node::insert(const triangle_t& tria) {
+    if (is_zero(space_degree_)) {
+        return ;
+    }
+
+    if (!are_ptrs_set_) {
+        set_pointers();
+        are_ptrs_set_ = true;
+    }
+
+    for (size_type count = 0; count < VOLUMES_NUMBER; ++count) {
+        
+        if (is_tria_inside(tria)) {
+            
+        }
+    }
+}
+
+Node::Node(const Node& parent, const point_t& center, double space_degree):
+            BoundingCube(center, space_degree),
+            parent_(parent) {}
+
 
 OctTree::const_value_type& OctTree::get_root_node() const noexcept {
     return root_node_;
@@ -26,3 +64,4 @@ void OctTree::insert_triangle(const triangle_t& tria) {
 }
 
 } // <--- namespace spaceBreaking
+
