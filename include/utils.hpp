831c5c7a4e048f1f605be8a8d38c6d535b7322a1
Victor Berbenets <berbenets.vd@phystech.edu>
2023-09-21 10:46:46 +0300

    added missing std to fabs

--- include/utils.hpp
+++ include/utils.hpp
@@ -18,11 +18,11 @@ static constexpr double epsilon = 1e-6;
 inline bool are_equal(double val1, double val2,
                       double maxDiff    = epsilon,
                       double maxRelDiff = epsilon) {
-    double diff = fabs(val1 - val2);
+    double diff = std::fabs(val1 - val2);
     if (diff < maxDiff) { return true; }
 
-    val1 = fabs(val1);
-    val2 = fabs(val2);
+    val1 = std::fabs(val1);
+    val2 = std::fabs(val2);
     double largest = std::max(val1, val2);
 
     return diff < largest * maxRelDiff;
